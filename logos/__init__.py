d0bd08dec7c641bdedac67f68bde0266dfe1dce4
Sam Kasbawala <samarth.kasbawala@uconn.edu>
2021-08-14 18:53:31 -0400

    add header to logos package init file

--- logos/__init__.py
+++ logos/__init__.py
@@ -1,1 +1,14 @@
+"""This package contains a webscraper to download all of the logos for colleges that
+participate in NCAA. See the wiki for a more in depth explanation."""
+
+
+__author__ = "Sam Kasbawala"
+__credits__ = ["Sam Kasbawala"]
+
+__licence__ = "BSD"
+__maintainer__ = "Sam Kasbawala"
+__email__ = "samarth.kasbawala@uconn.edu"
+__status__ = "Development"
+
+
 from .logos import get_logos
